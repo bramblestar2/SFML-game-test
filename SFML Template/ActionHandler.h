aac749c253d35a1ede4a524df74137524bb4d5c2
Jay Feather2000 <thegals4321@gmail.com>
2022-11-12 23:20:56 -0600

    l

--- SFML Template/ActionHandler.h
+++ SFML Template/ActionHandler.h
@@ -2,19 +2,27 @@
 #include <vector>
 #include <iostream>
 
-enum ACTIONS { MOVE, BREAK, ATTACK };
+enum ACTIONS { MOVE, BREAK, ATTACK, DIRECTION_CHANGE };
 
 class ActionHandler
 {
 public:
 
 	void push_back(ACTIONS a) { actionEvents.push_back(a); }
-	bool poll_events() const { return !actionEvents.empty(); };
+	bool poll_events(ActionHandler a) const { return !a.actionEvents.empty(); };
 
-	ACTIONS getEvent() const { return actionEvents.at(actionEvents.size() - 1); }
+	ACTIONS getEvent(ActionHandler a) { 
+		remove_top_event(a);
+		return a.actionEvents.at(a.actionEvents.size() - 1); 
+	}
+
+	ACTIONS getEvent() {
+		remove_top_event(*this);
+		return actionEvents.at(actionEvents.size() - 1);
+	}
 
 private:
-	void remove_top_event() { actionEvents.pop_back(); }
+	void remove_top_event(ActionHandler a) { a.actionEvents.pop_back(); }
 
 	std::vector<ACTIONS> actionEvents;
 };
